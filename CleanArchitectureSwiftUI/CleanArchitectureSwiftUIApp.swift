2ae7b3683339404b13c8222828fa857777862ff4
Alexandre Robaert <robaert.developer.com>
2024-07-16 11:56:52 -0300

    Initial Commit

---
+++ CleanArchitectureSwiftUI/CleanArchitectureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureSwiftUIApp.swift
+//  CleanArchitectureSwiftUI
+//
+//  Created by Alexandre Robaert on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchitectureSwiftUITests/CleanArchitectureSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureSwiftUITests.swift
+//  CleanArchitectureSwiftUITests
+//
+//  Created by Alexandre Robaert on 16/07/24.
+//
+
+import Testing
+@testable import CleanArchitectureSwiftUI
+
+struct CleanArchitectureSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
